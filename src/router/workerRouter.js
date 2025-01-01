35c1cb6530e476abfe831964358cf64c58e2029f
Jeiver <119613594+jeiverlahilote@users.noreply.github.com>
2025-01-01 18:36:10 +0700

    menambahkan verfyToken

--- src/router/workerRouter.js
+++ src/router/workerRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import workerHandler from "../handlers/workerHandler.js";
+import { verifyToken } from "../utils/middleware/jwtAuth.js";
 
 const router = Router()
 
-router.post('/workers', workerHandler.createWorker);
-router.get("/workers", workerHandler.readAllWorkers);
-router.delete("/workers", workerHandler.deleteWorkerHandler);
+router.post('/workers',verifyToken, workerHandler.createWorker);
+router.get("/workers",verifyToken, workerHandler.readAllWorkers);
+router.delete("/workers",verifyToken, workerHandler.deleteWorkerHandler);
 
 
 export default {router}
